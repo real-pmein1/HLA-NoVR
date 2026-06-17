c581040bac59f7181e90887cc5138c39deadde1e
pmein1 <pmein1@protonmail.com>
2026-06-17 10:57:13 +0100

    a3_distillery - Fix the cellar door crank

--- game/hlvr/scripts/vscripts/useextra.lua
+++ game/hlvr/scripts/vscripts/useextra.lua
@@ -52,7 +52,7 @@ if map == "a3_distillery" then
     end
 end
 
-if not vlua.find(model, "doorhandle") and name ~= "greenhouse_door" and name ~= "russell_entry_window" and name ~= "4910_135_interactive_wheel" and name ~= "bell" and name ~= "interactive_wheel2" and name ~= "interactive_wheel" and name ~= "larry_ladder" and name ~= "@pod_shell" and name ~= "589_panel_switch" and name ~= "tc_door_control" and name ~= "2_203_elev_anim_door" and name ~= "pallet_lever_unpowered" and name ~= "pallet_lever_vertical" and name ~= "pallet_lever" and not (name == "intro_rollup_door" and map == "a3_distillery") and (class == "item_health_station_charger" or (class == "prop_animinteractable" and (not vlua.find(name, "elev_anim_door") or (vlua.find(name, "elev_anim_door") and thisEntity:Attribute_GetIntValue("toggle", 0) == 1 and thisEntity:GetVelocity() == Vector(0, 0, 0))) and not vlua.find(name, "5628_2901_barricade_door")) or (class == "item_hlvr_combine_console_rack" and IsCombineConsoleLocked() == false)) and not (map == "a4_c17_parking_garage" and name == "door_reset" and player:Attribute_GetIntValue("circuit_" .. map .. "_toner_junction_5_completed", 0) == 0) and thisEntity:Attribute_GetIntValue("used", 0) == 0 then
+if not vlua.find(model, "doorhandle") and name ~= "greenhouse_door" and name ~= "russell_entry_window" and name ~= "4910_135_interactive_wheel" and name ~= "bell" and name ~= "interactive_wheel2" and name ~= "interactive_wheel" and name ~= "larry_ladder" and name ~= "@pod_shell" and name ~= "589_panel_switch" and name ~= "tc_door_control" and name ~= "2_203_elev_anim_door" and name ~= "pallet_lever_unpowered" and name ~= "pallet_lever_vertical" and name ~= "pallet_lever" and name ~= "11478_6233_tutorial_wheel" and not (name == "intro_rollup_door" and map == "a3_distillery") and (class == "item_health_station_charger" or (class == "prop_animinteractable" and (not vlua.find(name, "elev_anim_door") or (vlua.find(name, "elev_anim_door") and thisEntity:Attribute_GetIntValue("toggle", 0) == 1 and thisEntity:GetVelocity() == Vector(0, 0, 0))) and not vlua.find(name, "5628_2901_barricade_door")) or (class == "item_hlvr_combine_console_rack" and IsCombineConsoleLocked() == false)) and not (map == "a4_c17_parking_garage" and name == "door_reset" and player:Attribute_GetIntValue("circuit_" .. map .. "_toner_junction_5_completed", 0) == 0) and thisEntity:Attribute_GetIntValue("used", 0) == 0 then
     if vlua.find(name, "slide_train_door") and Entities:FindByClassnameNearest("phys_constraint", thisEntity:GetCenter(), 20) then
         return
     end
@@ -1121,8 +1121,19 @@ if map == "a3_distillery" then
         end
     end
 
-    if name == "11478_6233_tutorial_wheel" then
-        SendToConsole("ent_fire 11478_6233_verticaldoor_wheel_tutorial open")
+    if name == "11478_6233_tutorial_wheel" then -- needs further work
+        completion_amount = 0
+        SendToConsole("ent_fire 11478_6233_tutorial_wheel EnableReturnToCompletion; ent_fire 11478_6233_tutorial_wheel setreturntocompletionstyle 1")
+        player:SetThink(function()
+            if player:Attribute_GetIntValue("use_released", 0) == 1 then
+                completion_amount = 0
+                SendToConsole("ent_fire 11478_6233_tutorial_wheel setreturntocompletionamount 0")
+            else
+                completion_amount = completion_amount + 0.05
+                SendToConsole("ent_fire 11478_6233_tutorial_wheel setreturntocompletionamount " .. completion_amount)
+                return 0.3
+            end
+        end, "Interacting", 0)
     end
 
     if name == "11479_2385_button_pusher_prop" then
